6b92c5d4215c2dfa679e4a77cd0da1fc64c31e35
Sarkhan <syako44@gmail.com>
2022-07-01 12:49:29 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -11,7 +11,7 @@ prompt.get('input', function (err, result) {
     var input = result.input; // "5,2 4 7 12 9"
     var input_data = input.split(','); // ['5', '2 4 7 12 9']
     var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
-    var arr = input_data[1].split(' '); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
+    var arr = input_data[1].split(','); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
     let arrnew = []
 
 for(i = 0 ; i < arr.length ; i++){
